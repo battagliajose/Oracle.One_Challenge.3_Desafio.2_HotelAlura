ae76f613bb2998f48fbecb4eafb185708127a649
battagliajose <battagliajose@gmail.com>
2022-10-04 23:05:56 -0300

    Reservas DAO y Controller - Create y Delete

---
+++ src/model/Reserva.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.sql.Date;
+
+public class Reserva {
+	
+	private Integer id;
+	private Date fechaEntrada;
+	private Date fechaSalida;
+	private int valor;
+	private String formaDePago;
+	
+	public Reserva(Date fechaEntrada, Date fechaSalida, int valor, String formaDePago) {
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
+		this.valor = valor;
+		this.formaDePago = formaDePago;
+	}
+	
+	public Reserva(Integer id, Date fechaEntrada, Date fechaSalida, int valor, String formaDePago) {
+		this.id = id;
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
+		this.valor = valor;
+		this.formaDePago = formaDePago;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public Date getFechaEntrada() {
+		return fechaEntrada;
+	}
+	
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+	
+	public String getFormaDePago() {
+		return formaDePago;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/test/testConexion.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import controller.ReservasController;
+
+import java.sql.Date;
+
+import dao.ReservaDAO;
+import factory.ConnectionFactory;
+import model.Reserva;
+
+public class testConexion {
+
+	public static void main(String[] args) throws ParseException {
+		ReservasController reservasController = new ReservasController();
+
+		
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		
+		Date fechaEntrada = new java.sql.Date(df.parse("03-01-2023").getTime());
+		Date fechaSalida = new java.sql.Date(df.parse("15-01-2023").getTime());
+		
+		reservasController.create(new Reserva(fechaEntrada, fechaSalida, 1500, "Efectivo"));
+		
+		reservasController.delete(7);
+		
+	}
+
+}
